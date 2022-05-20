78078173c186ee607f876033753b775295af3d4a
Karina1Karina <chobotar.karina@gmail.com>
2022-05-20 15:17:08 +0300

    add class Beauty Salon

--- beauty_salon.py
+++ beauty_salon.py
@@ -52,3 +52,29 @@ class NailsMaster(Person):
         process_of_manicure = {"Correction": 100, "Nail extension": 200, "Gel coating": 150}
         price = reduce(lambda x, y: x + y, process_of_manicure.values())
         return price
+
+
+class BeautySalon:
+    count_clients = 0
+    suma = 0
+
+    def __init__(self):
+        BeautySalon.count_clients +=1
+        BeautySalon.suma = 0
+    
+    @classmethod
+    def return_count(cls):
+        return f"Count of beauty salon clients: {cls.count_clients}"
+    
+    @staticmethod
+    def description():
+        return "This salon provides the following services: manicure, makeup."
+
+    def makeup_procedure(self, makeup: Beautician):
+        BeautySalon.suma += makeup.do_makeup()
+
+    def manicure_procedure(self, manicure: NailsMaster):
+        BeautySalon.suma += manicure.do_manicure()
+    
+    def cost(self):
+        return f"Final cost: {BeautySalon.suma}$"
